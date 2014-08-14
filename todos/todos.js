3c2f0fa399ca2b7c1d536079d8d903dd30088775
James Kahng <jameskahng@gmail.com>
2014-08-14 03:01:38 -0700

    started todo

---
+++ todos/todos.js
@@ -0,0 +1,17 @@
+$(function  () {
+    var Todo = Backbone.Model.extend({
+        defaults: function  () {
+            return {
+                title: 'empty todo...',
+                order: Todos.nextOrder(),
+                done: false
+            };
+        },
+
+        toggle: function  () {
+            this.save({done: !this.get('done')});
+        }
+    });
+
+    
+})
